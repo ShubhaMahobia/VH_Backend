bdabc844efb49e3465bf0e0016a0a78e306c2eca
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-03-16 14:56:50 +0530

    added Comments

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -2,6 +2,8 @@ const express = require("express");
 const userPatient = require("../model/UserPatientModel");
 const bcrypt = require("bcryptjs");
 
+
+//This is a test function to check is the server is running or not.
 exports.test = (req, res) => {
   try {
     return res
@@ -12,6 +14,7 @@ exports.test = (req, res) => {
   }
 };
 
+//This is the API to check if the user is already registered or not in our system.
 exports.isNewUser = async (req, res) => {
   try {
     const isUserExist = await userPatient.findOne({
@@ -31,14 +34,18 @@ exports.isNewUser = async (req, res) => {
   }
 };
 
+//This is the create Profile Function for the user(Patient)
 exports.signUpPatient = async (req, res) => {
   try {
+
+    //Here we are saving the identification number in encrypted form so that it will be secured in our database
     const identificationNumber = req.body.identificationNumber;
     const hashedIdentificationNumber = await bcrypt.hash(
       identificationNumber,
       12
     );
 
+  //Creating patient Object 
     const patient = new userPatient({
       firebaseUserId: req.body.firebaseUserId,
       firstName: req.body.firstName,
@@ -61,8 +68,7 @@ exports.signUpPatient = async (req, res) => {
         message: "Identification Number Already Exist in Database",
       });
     }
-
-    await patient.save();
+    await patient.save(); //Saving command for saving user to database
     return res.status(200).json({
       success: true,
       message: "User data saved successfully",
